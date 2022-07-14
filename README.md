cee0a4857ce7f2e0deec5ee836de58919ca424a6
samarth <samarthchoprapws@gmail.com>
2022-07-15 02:45:57 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+<<<<<<< HEAD
 # Hardhat-Fund-Me
 Learning about Smart Contracts, and using cool tools like Hardhat, waffle, etherscan API and more using JavaScript.
+=======
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+GAS_REPORT=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+>>>>>>> initial commit
---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,34 @@
+const { getNamedAccounts, deployments, network } = require("hardhat");
+const { networkConfig, developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId;
+
+    let ethUsdPriceFeedAddress;
+    if (chainId == 31337) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUsdPriceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    }
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], // put price feed adress
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        // verify
+        await verify(fundMe.address, [ethUsdPriceFeedAddress]);
+    }
+
+    log("----------------------------------------------------");
+}
+
+module.exports.tags = ["all", "fundme"]
